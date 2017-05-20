868a0d8003fd9b13d64658702202f660107df346
Oliver Allen <me@oliverallen.co.uk>
2017-05-20 21:52:31 +0100

    test

--- src/main/java/me/okx/neim/token/TokenManager.java
+++ src/main/java/me/okx/neim/token/TokenManager.java
@@ -61,6 +61,8 @@ public class TokenManager {
         tokens.put("𝐓", new Factorial());
         tokens.put("𝐔", new UniquePrimeFactors());
 
+        int y = 0;
+
         tokens.put("𝐝", new Deltas());
         tokens.put("𝐠", new Largest());
         tokens.put("𝐣", new Join());
--- src/main/java/me/okx/neim/token/tokens/IsPalindrome.java
+++ src/main/java/me/okx/neim/token/tokens/IsPalindrome.java
@@ -1,13 +1,13 @@
 package me.okx.neim.token.tokens;
 
-import com.sun.deploy.config.VerboseDefaultConfig;
 import me.okx.neim.stack.NStack;
 import me.okx.neim.stack.NStackBuilder;
 import me.okx.neim.token.types.Monad;
+import me.okx.neim.token.types.vectorisable.Vectorisable;
 import me.okx.neim.util.Util;
 import me.okx.neim.var.VarInteger;
 
-public class IsPalindrome extends VerboseDefaultConfig implements Monad<VarInteger> {
+public class IsPalindrome extends Vectorisable implements Monad<VarInteger> {
 
     @Override
     public NStack monad(VarInteger a) {
