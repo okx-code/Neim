888a6f24d0fc386b335d21d852b82627201e4376
okx-code <okx-code@users.noreply.github.com>
2017-05-22 10:41:23 +0100

    Update NStack.java

--- src/main/java/me/okx/neim/stack/NStack.java
+++ src/main/java/me/okx/neim/stack/NStack.java
@@ -1,10 +1,12 @@
 package me.okx.neim.stack;
 
+import lombok.Getter;
 import me.okx.neim.util.InputUtil;
 
 import java.util.Stack;
 
 public class NStack extends Stack {
+    @Getter
     private InputUtil input;
 
     public NStack(InputUtil input) {
