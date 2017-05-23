ea51ee8b7b7c39d153b5b660749899be9b2b9d6d
Oliver Allen <me@oliverallen.co.uk>
2017-05-23 19:56:01 +0100

    make append cooler

--- src/main/java/me/okx/neim/token/tokens/dyad/Append.java
+++ src/main/java/me/okx/neim/token/tokens/dyad/Append.java
@@ -3,15 +3,24 @@ package me.okx.neim.token.tokens.dyad;
 import me.okx.neim.stack.NStack;
 import me.okx.neim.stack.NStackBuilder;
 import me.okx.neim.token.types.Dyad;
-import me.okx.neim.token.types.vectorisable.VectorisableDyadListInt;
 import me.okx.neim.var.IntList;
 import me.okx.neim.var.VarInteger;
 
-public class Append extends VectorisableDyadListInt implements Dyad<IntList, VarInteger> {
+public class Append implements Dyad<Object, Object> {
 
     @Override
-    public NStack dyad(IntList a, VarInteger b) {
-        a.add(b);
-        return new NStackBuilder(a).build();
+    public NStack dyad(Object a, Object b) {
+        IntList result = new IntList();
+        if(a instanceof VarInteger) {
+            result.add((VarInteger) a);
+        } else {
+            result.addAll((IntList) a);
+        }
+        if(b instanceof VarInteger) {
+            result.add((VarInteger) b);
+        } else {
+            result.addAll((IntList) b);
+        }
+        return new NStackBuilder(result).build();
     }
 }
