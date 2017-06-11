7cf71d9fc35909e1226a3e83f5a9642ce86ac662
Oliver Allen <me@oliverallen.co.uk>
2017-06-11 13:24:57 +0100

    fix subtract token

--- src/main/java/me/okx/neim/token/tokens/dyad/Subtract.java
+++ src/main/java/me/okx/neim/token/tokens/dyad/Subtract.java
@@ -3,9 +3,10 @@ package me.okx.neim.token.tokens.dyad;
 import me.okx.neim.stack.NStack;
 import me.okx.neim.stack.NStackBuilder;
 import me.okx.neim.token.types.Dyad;
+import me.okx.neim.token.types.vectorisable.VectorisableDyadIntInt;
 import me.okx.neim.var.VarInteger;
 
-public class Subtract implements Dyad<VarInteger, VarInteger> {
+public class Subtract extends VectorisableDyadIntInt implements Dyad<VarInteger, VarInteger> {
 
     @Override
     public NStack dyad(VarInteger a, VarInteger b) {
