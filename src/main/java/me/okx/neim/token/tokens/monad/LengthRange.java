b009c2618edb989b5882fcda0a02038a5891d8d5
Oliver Allen <me@oliverallen.co.uk>
2017-06-15 16:56:20 +0100

    fix lengthrange

--- src/main/java/me/okx/neim/token/tokens/monad/LengthRange.java
+++ src/main/java/me/okx/neim/token/tokens/monad/LengthRange.java
@@ -3,13 +3,13 @@ package me.okx.neim.token.tokens.monad;
 import me.okx.neim.stack.NStack;
 import me.okx.neim.stack.NStackBuilder;
 import me.okx.neim.token.types.Monad;
+import me.okx.neim.util.Util;
 import me.okx.neim.var.IntList;
-import me.okx.neim.var.VarInteger;
 
 public class LengthRange implements Monad<IntList> {
 
     @Override
     public NStack monad(IntList a) {
-        return new NStackBuilder(new VarInteger(a.size())).build();
+        return new NStackBuilder(Util.range(a.size())).build();
     }
 }
