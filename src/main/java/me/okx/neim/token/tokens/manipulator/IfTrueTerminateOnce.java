056b01e9d7b57056cdea08c441b8be6a1457b288
Oliver Allen <me@oliverallen.co.uk>
2017-06-27 19:12:32 +0100

    fix if true terminate silent

--- src/main/java/me/okx/neim/token/tokens/manipulator/IfTrueTerminateOnce.java
+++ src/main/java/me/okx/neim/token/tokens/manipulator/IfTrueTerminateOnce.java
@@ -3,12 +3,23 @@ package me.okx.neim.token.tokens.manipulator;
 import me.okx.neim.stack.NStack;
 import me.okx.neim.token.TokenManager;
 import me.okx.neim.token.types.Manipulator;
+import me.okx.neim.var.IntList;
+import me.okx.neim.var.VarInteger;
 
 public class IfTrueTerminateOnce implements Manipulator {
 
     @Override
     public NStack manipulator(NStack stack, TokenManager tm) {
-        tm.finishSilent = true;
+        Object o = stack.pop();
+        boolean terminate = false;
+        if(o instanceof VarInteger) {
+            terminate = o.equals(new VarInteger(1));
+        } else if(o instanceof IntList) {
+            terminate = ((IntList) o).contains(new VarInteger(1));
+        }
+        if(terminate) {
+            tm.finishSilent = true;
+        }
         return stack;
     }
 }
