df1723ad1d8b9bf2be98537d55616bf165d15d6f
Oliver Allen <me@oliverallen.co.uk>
2017-06-27 17:58:51 +0100

    fix power to work with bigintegers

--- src/main/java/me/okx/neim/token/tokens/dyad/Power.java
+++ src/main/java/me/okx/neim/token/tokens/dyad/Power.java
@@ -6,12 +6,21 @@ import me.okx.neim.token.types.Dyad;
 import me.okx.neim.token.types.vectorisable.VectorisableDyadIntInt;
 import me.okx.neim.var.VarInteger;
 
+import java.math.BigInteger;
+
 public class Power extends VectorisableDyadIntInt implements Dyad<VarInteger, VarInteger> {
 
     @Override
     public NStack dyad(VarInteger a, VarInteger b) {
+
         return new NStackBuilder(
-                new VarInteger((long) Math.pow(a.getValue(), b.getValue()))
+                new VarInteger(pow(a.getBigIntegerValue(), b.getBigIntegerValue()))
         ).build();
+    }
+
+    // recursion is bad but who cares
+    private BigInteger pow(BigInteger base, BigInteger power) {
+        if(power.compareTo(BigInteger.ZERO) == 0) return BigInteger.ONE;
+        return base.multiply(pow(base, power.subtract(BigInteger.ONE)));
     }
 }
