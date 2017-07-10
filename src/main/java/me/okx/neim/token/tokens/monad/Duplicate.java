417b2740bd849f2b1e9f925bbd7e48d773daff3f
Oliver Allen <me@oliverallen.co.uk>
2017-07-10 16:48:39 +0100

    fix duplicat

--- src/main/java/me/okx/neim/token/tokens/monad/Duplicate.java
+++ src/main/java/me/okx/neim/token/tokens/monad/Duplicate.java
@@ -3,11 +3,19 @@ package me.okx.neim.token.tokens.monad;
 import me.okx.neim.stack.NStack;
 import me.okx.neim.stack.NStackBuilder;
 import me.okx.neim.token.types.Monad;
+import me.okx.neim.var.IntList;
+import me.okx.neim.var.VarInteger;
 
 public class Duplicate implements Monad<Object> {
 
     @Override
     public NStack monad(Object a) {
-        return new NStackBuilder(a).add(a).build();
+        Object b = null;
+        if(a instanceof IntList) {
+            b = ((IntList) a).clone();
+        } else if(a instanceof VarInteger) {
+            b = ((VarInteger) a).clone();
+        }
+        return new NStackBuilder(a).add(b).build();
     }
 }
